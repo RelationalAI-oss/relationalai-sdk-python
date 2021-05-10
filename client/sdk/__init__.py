831b5d0b4da051cbc1f532762d49c9da7079e8be
helmi.nour <helmiinour@gmail.com>
2021-05-10 20:39:25 +0100

    added raicredentials

--- client/sdk/__init__.py
+++ client/sdk/__init__.py
@@ -1,3 +1,4 @@
 from sdk.connection import Connection
 from sdk.local_connection import LocalConnection
 from sdk.delve_client import DelveClient
+from sdk.rai_credentials import RAICredentials
---
+++ client/sdk/rai_credentials.py
@@ -0,0 +1,22 @@
+import configparser
+import json
+
+from pathlib import Path
+
+class RAICredentials(object):
+    def __init__(self, profile:str="default", config_path:str=str("{}/.rai/config".format(Path.home()))):
+        self.parse_config(profile=profile, config_path=config_path)
+
+    def parse_config(self, profile:str="default", config_path:str=str("{}/.rai/config".format(Path.home()))):
+        config = configparser.ConfigParser()
+        config.read(config_path)
+
+        self.region = config[profile]["region"]
+        self.host = config[profile]["host"]
+        self.port = config[profile]["port"]
+        
+        private_key_filename = config[profile]["private_key_filename"]
+        private_key_file_content = Path("{}/{}".format(Path(config_path).parent, private_key_filename)).read_text()
+
+        self.private_key = json.loads(private_key_file_content)["sodium"]["seed"]
+        self.access_key = config[profile]["access_key"]
