1b1fb34ed3cfd700749ffcb6bd426c330df0f460
helmi.nour <helmiinour@gmail.com>
2021-05-21 03:30:00 +0100

    remove client from drv output.

--- default.nix
+++ default.nix
@@ -14,23 +14,23 @@ app = pkgs.poetry2nix.mkPoetryApplication {
 };
 
 in
-{
-  client = app.overrideAttrs(oldAttrs: rec {
-    name = "delve-client-${version}";
-    version = "1.1.3";
-    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ delveBinary ];
-    checkPhase = ''
-      mkdir home
-      export HOME=$PWD/home
 
-      delve server &
-      PID=$!
-      sleep 15s
+app.overrideAttrs(oldAttrs: rec {
+  name = "delve-client-${version}";
+  version = "1.1.3";
+  nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ delveBinary ];
+  checkPhase = ''
+    mkdir home
+    export HOME=$PWD/home
 
-      pytest integration_tests || (kill -9 $PID && exit 1)
-      echo "Shutting down delve server. Pid: $PID"
-      kill -9 $PID
-    '';
-    doCheck = doCheck;
-  });
-}
+    delve server &
+    PID=$!
+    sleep 15s
+
+    pytest integration_tests || (kill -9 $PID && exit 1)
+    echo "Shutting down delve server. Pid: $PID"
+    kill -9 $PID
+  '';
+  doCheck = doCheck;
+})
+
